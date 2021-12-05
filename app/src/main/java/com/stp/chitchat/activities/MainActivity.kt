cddda523a0fbc301bbd20a746c08b892c85d2abb
Akshay Khare <akhare931@gmail.com>
2021-12-05 19:36:22 +0530

    Working with navigation graph in main activity

--- app/src/main/java/com/stp/chitchat/activities/MainActivity.kt
+++ app/src/main/java/com/stp/chitchat/activities/MainActivity.kt
@@ -2,11 +2,20 @@ package com.stp.chitchat.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.stp.chitchat.R
+import com.stp.chitchat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavBar.setupWithNavController(findNavController(R.id.dashboard_fragment_view))
     }
 }
